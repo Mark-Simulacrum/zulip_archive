3f31481aa21b35fafd3cbf4178caa4fb529d15f5
Mark Rousskov <mark.simulacrum@gmail.com>
2021-08-07 19:10:47 -0400

    add new script

---
+++ generate-old.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+URL=/stream/268952-edition-2021/topic/.22Rust.202021.20dry.20run.22.20crater.20run.html
+mkdir 268952edition2021
+echo "<meta http-equiv=\"Refresh\" content=\"0; url=$URL\" />" > 268952edition2021/01650Rust2021dryruncraterrun.html
